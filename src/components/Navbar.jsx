ec3276b59a5ee39c5f00fa0db9c2a58e5f766de4
ABIN VARGHEXE <abinvarghese092005@gmail.com>
2024-08-27 00:13:06 +0530

    chore: Update navigation links and icons (Navbar)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { appleImg, bagImg, searchImg } from '../utils';
+import { IncialLogo, MailImg, WhatsAppImg } from '../utils';
 import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav className="flex w-full screen-max-width">
-        <img src={appleImg} alt="Apple" width={14} height={18} />
+        <img src={IncialLogo} alt="Apple" width={14} height={18} />
 
         <div className="flex flex-1 justify-center max-sm:hidden">
           {navLists.map((nav) => (
@@ -16,8 +16,8 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-          <img src={searchImg} alt="search" width={18} height={18} />
-          <img src={bagImg} alt="bag" width={18} height={18} />
+          <img src={WhatsAppImg} alt="bag" width={20} height={20} className="cursor-pointer text-gray hover:opacity-15 transition-all" />
+          <img src={MailImg} alt="search" width={25} height={20} className=" cursor-pointer text-gray hover:opacity-15 transition-all" />
         </div>
       </nav>
     </header>
--- src/utils/index.js
+++ src/utils/index.js
@@ -11,9 +11,9 @@ import highlightFourthmv from "/assets/videos/hightlight-fourth.mp4";
 import exploremv from "/assets/videos/explore.mp4";
 import framemv from "/assets/videos/frame.mp4";
 
-import apple from "/assets/images/apple.svg";
-import search from "/assets/images/search.svg";
-import bag from "/assets/images/bag.svg";
+import Incial from "/assets/images/incial-logo.svg";
+import Mail from "/assets/images/mail.svg";
+import WhatsApp from "/assets/images/whatsapp.svg";
 import watch from "/assets/images/watch.svg";
 import right from "/assets/images/right.svg";
 import replay from "/assets/images/replay.svg";
@@ -38,9 +38,9 @@ export const highlightFourthVideo = highlightFourthmv;
 export const exploreVideo = exploremv;
 export const frameVideo = framemv;
 
-export const appleImg = apple;
-export const searchImg = search;
-export const bagImg = bag;
+export const IncialLogo = Incial;
+export const MailImg = Mail;
+export const WhatsAppImg = WhatsApp;
 export const watchImg = watch;
 export const rightImg = right;
 export const replayImg = replay;
