bdb1dc281999efcb9bc6361082db0adca4a5d4d4
ABIN-VARGEXE <abinvarghese2027@cs.ajce.in>
2025-02-16 11:43:36 +0530

    Update favicon and implement loading screen in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar';
+import { useState, useEffect } from 'react';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 import Model from './components/Model';
@@ -10,18 +11,34 @@ import * as Sentry from '@sentry/react';
 import Loading from './components/Loading';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+
   return (
-    <main className="bg-black ">
-      {/* <Navbar />
-      <Hero />
-      <Highlights /> */}
-      {/* <Model /> */}
-      {/* <Features />
-      <HowItWorks /> */}
-      <Loading />
-      {/* <Footer /> */}
-    </main>
-  )
-}
+    <>
+      <div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <main className="bg-black">
+            <Navbar />
+            <Hero />
+            <Highlights />
+            <Model />
+            <Features />
+            <HowItWorks />
+            <Footer />
+          </main>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Sentry.withProfiler(App);
